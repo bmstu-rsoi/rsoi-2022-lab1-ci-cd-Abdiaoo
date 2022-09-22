7fc12bcf38eb2fcc2b5870ed428a0d23b21800bf
Abdi <112772257+Abdiaoo@users.noreply.github.com>
2022-09-22 13:46:28 +0300

    Update settings.py

--- src/Personnes/settings.py
+++ src/Personnes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aa=^*yh_(8wal^k9q1r!_@q#b5gt74qtm5re8n77b@+i&k64f!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
